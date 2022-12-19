3a323a766cb658675b4267e262a9df94c5b846f6
Mikołaj Waluśkiewicz <mikolaj.waluskiewicz@gmail.com>
2022-12-19 09:49:16 +0100

    feat: added tests to GetUSerBudgets method

---
+++ FamilyBudget/Server.Tests/ServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using Bogus;
+using Duende.IdentityServer.EntityFramework.Options;
+using FamilyBudget.Server.Data;
+using FamilyBudget.Server.Models;
+using FamilyBudget.Server.Services.Identity;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace FamilyBudget.Server.Tests
+{
+    public class ServiceTest
+    {
+        private const string InMemoryConnectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _connection;
+
+        protected readonly string UserId = Guid.NewGuid().ToString();
+        protected readonly IUserProvider UserProvider;
+        protected readonly ApplicationDbContext DbContext;
+
+        protected ServiceTest()
+        {
+            _connection = new SqliteConnection(InMemoryConnectionString);
+            _connection.Open();
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlite(_connection)
+                    .Options;
+
+            var operationalStoreOptions = Options.Create(new OperationalStoreOptions());
+
+            DbContext = new ApplicationDbContext(options, operationalStoreOptions);
+
+            UserProvider = Substitute.For<IUserProvider>();
+            UserProvider.UserId.Returns(UserId);
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            DbContext.Database.EnsureCreated();
+
+            var user = new Faker<ApplicationUser>()
+                .RuleFor(x => x.Id, UserId)
+                .Generate();
+
+            DbContext.Add(user);
+            DbContext.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DbContext.Database.EnsureDeletedAsync();
+        }
+
+        protected async Task<ApplicationUser> GetMockedUser() =>
+            await DbContext.Users.FindAsync(UserId);
+    }
+}
---
+++ FamilyBudget/Server.Tests/Services/Budgets/BudgetServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using Bogus;
+using FamilyBudget.Server.Models;
+using FamilyBudget.Server.Services.Budget;
+using NUnit.Framework;
+
+namespace FamilyBudget.Server.Tests.Services.Budgets
+{
+    [TestFixture]
+    public class BudgetServiceTests : ServiceTest
+    {
+        private readonly BudgetService _sut;
+
+        public BudgetServiceTests()
+        {
+            _sut = new BudgetService(DbContext, UserProvider);
+        }
+
+        [Test]
+        public async Task GetUserBudgets_ShouldReturnAllUserBudgetsWithProperties()
+        {
+            //Arrange
+            int budgetsCount = 5;
+
+            var user = await GetMockedUser();
+
+            var budgetAssignedUsers = new List<ApplicationUser> { user };
+
+            var userBudgets = new Faker<Budget>()
+                .RuleFor(x => x.Name, f => f.Finance.AccountName())
+                .RuleFor(x => x.Balance, f => f.Random.Decimal())
+                .RuleFor(x => x.UsersAssignedToBudget, budgetAssignedUsers)
+                .Generate(budgetsCount);
+
+            await DbContext.AddRangeAsync(userBudgets);
+            await DbContext.SaveChangesAsync();
+
+            //Act
+            var result = await _sut.GetUserBudgets();
+
+            //Assert
+            Assert.AreEqual(budgetsCount, result.Count);
+
+            foreach (var budget in userBudgets)
+            {
+                Assert.IsTrue(result.Any(x => x.Id == budget.Id && x.Name == budget.Name && x.Balance == budget.Balance));
+            }
+        }
+
+        [Test]
+        public async Task GetUserBudgets_ShouldNotReturnOtherUsers()
+        {
+            //Arrange
+
+            var otherUserBudgets = new Faker<Budget>()
+                .RuleFor(x => x.Name, f => f.Finance.AccountName())
+                .RuleFor(x => x.Balance, f => f.Random.Decimal())
+                .Generate(10);
+
+            await DbContext.AddRangeAsync(otherUserBudgets);
+            await DbContext.SaveChangesAsync();
+
+            //Act
+            var result = await _sut.GetUserBudgets();
+
+            //Assert
+            Assert.AreEqual(0, result.Count);
+        }
+    }
+}
